c8a6f3a29060a1a62e889c4277832206a2f41ebf
Maxim Razdorozhnyi <rmv_93@mail.ru>
2024-07-09 20:55:28 +0300

    Starting new pet project

---
+++ messages.py
@@ -0,0 +1,23 @@
+# errors
+MAIN_ERROR = 'Ошибка работы программы'
+SEND_FAILED = 'Не удалось отправить сообщение'
+RESPONSE_NOT_DICT = 'Ответ от API не является словарем'
+RESPONSE_NOT_LIST = 'Ответ от API не является списком'
+RESPONSE_EMPTY = 'Ответ от API пуст'
+KEY_MISSING = 'Отсутствует ключ'
+TOKEN_MISSING = 'Отсутствует токен'
+STATUS_INCORRECT = 'Некорректный статус работы'
+RESP_CODE_FAILED = 'Некорректный код ответа'
+
+# info
+SEND = 'Отправка сообщения'
+SENDED = 'Сообщение отправлено'
+API_REQUEST = 'Запрос к API'
+CHECK_RESPONSE = 'Проверка соответствия ответа'
+STARTING = 'Начало работы'
+END_CHECK = 'Завершение проверки'
+PARSING = 'Начало парсинга'
+END_PARSE = 'Завершение парсинга'
+CHECKING_TOKENS = 'Проверка токенов'
+WAITS = 'Ожидание обновления'
+STATUS_CHANGED = 'Статус работы изменился'
