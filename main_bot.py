db9ef977414e76d4989834b469b6fff3f3069990
Maxim Razdorozhnyi <rmv_93@mail.ru>
2024-07-10 22:45:30 +0300

    add send message, check users funcs

--- main_bot.py
+++ main_bot.py
@@ -2,11 +2,11 @@ import logging
 import os
 import sys
 import time
-from http import HTTPStatus
+# from http import HTTPStatus
 
-import requests
+# import requests
 from dotenv import load_dotenv
-from telebot import TeleBot
+from telebot import TeleBot  # type: ignore
 
 from my_pet_bot import exceptions as exc
 from my_pet_bot import messages as msg
@@ -15,21 +15,20 @@ load_dotenv()
 
 CATS_API_KEY = os.getenv('CATS_API_KEY')
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+CHAT_ID_MAX = os.getenv('CHAT_ID')
 
 CAT_PIC_ENDPOINT = 'https://api.thecatapi.com/v1/images/search'
 
 
-def check_dotenv():
+def check_tokens():
     """Check that all necessary envs are present."""
     tokens = {
         'CATS_API_KEY': CATS_API_KEY,
-        'TELEGRAM_BOTTOKEN': TELEGRAM_BOT_TOKEN,
-        'CHAT_ID': CHAT_ID,
+        'TELEGRAM_BOT_TOKEN': TELEGRAM_BOT_TOKEN,
     }
 
     report_list = []
-    
+
     for name, token in tokens.items():
         if not token:
             report_list.append(name)
@@ -39,26 +38,41 @@ def check_dotenv():
         )
         raise exc.CheckTokensEnvFailure(msg.TOKEN_MISSING)
 
-    
+
+def check_users():
+    """Check added users."""
+    logging.info('start checking users')
+    users = {
+        'CHAT_ID_MAX': CHAT_ID_MAX,
+    }
+    for user, token in users.items():
+        if not token:
+            logging.info(f'{user} is not added')
 
 
+# TODO:  do messages func
+def send_message(bot, message):
+    """Send message to telegramm chat."""
+    bot.send_message(CHAT_ID_MAX, message)
+    logging.info(msg.SENDED)
+
+
+# FIXME:
 def main():
-    
+    """pass."""
     logging.info(msg.CHECKING_TOKENS)
-    check_dotenv()
+    check_tokens()
     logging.info(msg.END_CHECK)
 
     bot = TeleBot(TELEGRAM_BOT_TOKEN)
 
     timestamp = int(time.time())
 
-
-
-
+    # while True:
+    #     try:
 
 
 if __name__ == '__main__':
-    
     log_format = (
         '%(asctime)s [%(levelname)s]'
         '(%(filename)s).%(funcName)s:%(lineno)d - %(message)s'
@@ -68,5 +82,5 @@ if __name__ == '__main__':
         level=logging.DEBUG,
         handlers=[logging.StreamHandler(sys.stdout)],
     )
-    
+
     main()
