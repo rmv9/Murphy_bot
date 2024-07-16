39e4afb657ab941d11f8add4e77165ee01c22094
Maxim Razdorozhnyi <rmv_93@mail.ru>
2024-07-16 14:16:50 +0300

    Modify react on requests

--- main_bot.py
+++ main_bot.py
@@ -1,7 +1,6 @@
 """test mode."""
 import logging
 import sys
-import time
 
 from telebot import TeleBot, types  # type: ignore
 
@@ -10,7 +9,9 @@ import messages as msg
 import permissions as perm
 
 RETRY_PERIOD = 30
+RETRY_MODE = False
 
+bot = TeleBot(token=perm.TELEGRAM_BOT_TOKEN)
 
 # TODO: need to do something with logging.
 def main():
@@ -21,20 +22,54 @@ def main():
         sys.exit()
     logging.info(msg.END_CHECK)
 
-    bot = TeleBot(token=perm.TELEGRAM_BOT_TOKEN)
-
-    func.send_message(
-        bot,
-        (
-            'Bot is working. TEST mode\n'
-            f'Retry period is {RETRY_PERIOD} sec.'
+    @bot.message_handler(commands=['start'])
+    def wake_up(message):
+        """pass."""
+        chat = message.chat
+        name = message.chat.first_name
+        # Создаём объект клавиатуры:
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.row(  # Первая строка кнопок.
+            types.KeyboardButton('тут пока пусто'),  # Первую кнопку в строке.
+            types.KeyboardButton('Пришли собачку'),
         )
-    )
+        keyboard.row(  # Вторая строка кнопок.
+            types.KeyboardButton('Покажи параметры'),
+            types.KeyboardButton('Пришли котика'),
+        )
+        bot.send_message(
+            chat_id=chat.id,
+            text=(
+                f'Привет {name}, меня зовут Murpy и пока что я только учусь :)\n'
+                'Но я уже могу отправлять тебе картинки с животными.\n\n'
+                'Если будет надо, то в Параметрах я '
+                'могу показать информацию по моей работе\n'
+                'В поле chat id будет указан id твоего аккаунта Телеграм\n'
+                ''
+            ),
+            reply_markup=keyboard,  # Отправляем клавиатуру в сообщении бота.
+        )
 
-    while True:
-        # send_message(bot, 'a simple test message')
-        func.send_photo(bot, func.get_pic())
-        time.sleep(RETRY_PERIOD)
+    @bot.message_handler(content_types=['text'])
+    def react(message):
+        """pass."""
+        chat = message.chat
+        if message.text == 'Пришли котика':
+            bot.send_photo(chat.id, func.get_cat())
+        elif message.text == 'Пришли собачку':
+            bot.send_photo(chat.id, func.get_dog())
+        elif message.text == 'Покажи параметры':
+            bot.send_message(
+                chat.id,
+                text=(
+                    'bot is working. TEST mode\n'
+                    f'retry period: {RETRY_PERIOD} sec.\n'
+                    f'retry mode: {RETRY_MODE}\n\n'
+                    f'your tg name: {chat.first_name}\n'
+                    f'chat id: {chat.id}\n'
+                    f'owner id: {perm.CHAT_ID_MAX}\n'
+                )
+            )
 
 
 # TODO: Lets cut log settings out of there
@@ -52,3 +87,4 @@ if __name__ == '__main__':
     )
 
     main()
+    bot.polling()
