775318efeec4e2399c431dd01bee11f43d393446
Maxim Razdorozhnyi <rmv_93@mail.ru>
2024-08-06 15:14:34 +0300

    Add info and afisha funcs

--- main_bot.py
+++ main_bot.py
@@ -1,10 +1,24 @@
 """test mode."""
 import logging
 import sys
+import os
 
-import functions as func
-import messages as msg
-import permissions as perm
+from dotenv import load_dotenv
+
+import core.functions as func
+import helpers.messages as msg
+import core.permissions as perm
+
+load_dotenv()
+
+CHAT_ID_DARYA = os.getenv('CHAT_ID_DARYA')
+CHAT_ID_MAX = os.getenv('CHAT_ID_MAX')
+CHAT_ID_ALEX = os.getenv('CHAT_ID_ALEX')
+
+weather_list = ['today', 'tomorrow']
+pic_list = ['cat', 'dog']
+info_list = ['functions', 'params', 'author']
+afisha_list = ['mos_afisha', 'cinema_balt', 'cinema_otrada']
 
 
 # TODO: need to do something with logging.
@@ -15,15 +29,29 @@ def main():
         logging.info(f'Corrupted tokens: {perm.check_tokens()}')
         sys.exit()
     logging.info(msg.info['END_CHECK'])
+    # func.direct_initialization(CHAT_ID_DARYA)
+    # func.direct_initialization(CHAT_ID_MAX)
+    # func.direct_initialization(CHAT_ID_ALEX)
 
-    # func.direct_initialization('CHAT_ID_MAX')
-    # func.direct_initialization('CHAT_ID_DARYA')
-    # func.direct_initialization('CHAT_ID_ALEX')
-    func.bot_v1.message_handler(commands=['start'])(func.start_menu)
-    func.bot_v1.message_handler(commands=['menu'])(func.main_menu)
-    func.bot_v1.message_handler(commands=['weather'])(func.weather_menu)
-    func.bot_v1.message_handler(commands=['info'])(func.info_menu)
-    func.bot_v1.message_handler(content_types=['text'])(func.react)
+    func.bot_v1.message_handler(commands=['start'])(func.init_cmd)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data == 'start')(func.start_menu)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data == 'pictures')(func.pics_menu)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data == 'afisha')(func.afisha_menu)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data == 'weather')(func.weather_menu)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data == 'info')(func.info_menu)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data in weather_list)(func.weather_react)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data in pic_list)(func.picture_react)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data in info_list)(func.info_react)
+    func.bot_v1.callback_query_handler(
+        func=lambda call: call.data in afisha_list)(func.afisha_react)
 
 
 # TODO: Lets cut log settings out of there
